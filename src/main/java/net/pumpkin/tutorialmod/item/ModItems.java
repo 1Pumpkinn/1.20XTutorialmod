ad1f41410b7cedaf095807070344a35d6dbcb12b
Pumpkin <wishsmp1002@gmail.com>
2024-12-05 01:20:29 +1100

    added metal detector episode 6 Kaupenjoe 1.20 tutorial

--- src/main/java/net/pumpkin/tutorialmod/item/ModItems.java
+++ src/main/java/net/pumpkin/tutorialmod/item/ModItems.java
@@ -1,7 +1,6 @@
 package net.pumpkin.tutorialmod.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
@@ -10,10 +9,15 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.pumpkin.tutorialmod.TutorialMod;
+import net.pumpkin.tutorialmod.item.custom.MetalDetectorItem;
 
 public class ModItems {
     public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings()));
     public static final Item RAW_RUBY = registerItem("raw_ruby", new Item(new FabricItemSettings()));
+
+    public static final Item METAL_DETECTOR = registerItem("metal_detector",
+            new MetalDetectorItem(new FabricItemSettings().maxDamage(64)));
+
 
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add(RUBY);
